2a7ddbe8382de3dd8f325d76faab789bec3f1b86
Leeerrrmmm <leeerrrmmm.@MacBook-Air-Leeerrrmmm.local>
2025-02-19 16:42:37 +0200

    add categody_prov

---
+++ lib/domain/category_provider.dart
@@ -0,0 +1,11 @@
+import 'package:nike_sneakers_store/data/category_model.dart';
+
+final List<CategoryModel> _categoryList = [
+  CategoryModel(title: 'All'),
+  CategoryModel(title: 'Outdoor'),
+  CategoryModel(title: 'Tennis'),
+  CategoryModel(title: 'Basketball'),
+  CategoryModel(title: 'football'),
+];
+
+List<CategoryModel> get categoryList => _categoryList;
